f440c92bfdad0ea46ae1cd5ccb6a63d99d78d8be
elisfromkirov <mozgg2002@gmail.com>
2022-12-23 17:10:41 +0300

    Add map generation

---
+++ Assets/Scripts/MapGeneration/Algorithms/MinSpanTree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MapGeneration.Algorithms
+{
+    public struct EdgeAdjacent
+    {
+        public int v_id;
+        public float weight;
+
+        public EdgeAdjacent(int v, float w)
+        {
+            v_id = v;
+            weight = w;
+        }
+    }
+
+    // public struct Edge
+    // {
+    //     public int u;
+    //     public int v;
+    //     public int weight;
+    // }
+
+    public class MinSpanTree
+    {
+        private static bool EdgeExists(List<EdgeAdjacent>[] graph, int v1, int v2, float weight)
+        {
+            var l = graph[v1];
+            foreach (var adjEdge in l)
+            {
+                if (adjEdge.v_id == v2)
+                {
+                    weight = adjEdge.weight;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static void MstInitialize(List<EdgeAdjacent>[] mst)
+        {
+            for (var i = 0; i < mst.Length; ++i)
+            {
+                mst[i] = new List<EdgeAdjacent>();
+            }
+        }
+        
+        public static List<EdgeAdjacent>[] FindMst(List<EdgeAdjacent>[] graph)
+        {
+            if (graph.Length < 1) throw new Exception("invalid graph passed to function");
+
+            var mst = new List<EdgeAdjacent>[graph.Length];
+            MstInitialize(mst);
+            var isVertexInMst = new bool[graph.Length];
+            for (var i = 0; i < isVertexInMst.Length; ++i)
+            {
+                isVertexInMst[i] = false;
+            }
+            isVertexInMst[0] = true;
+
+            for (var verticesInMstNumber = 1; verticesInMstNumber < graph.Length; ++verticesInMstNumber)
+            {
+                var edgeMinWeight = 100000.0f;
+                var minv1 = -1;
+                var minv2 = -1;
+
+                for (var vis_i = 0; vis_i < isVertexInMst.Length; ++vis_i)
+                {
+                    if (!isVertexInMst[vis_i]) continue;
+                    for (var unvis_i = 0; unvis_i < isVertexInMst.Length; ++unvis_i)
+                    {
+                        if (isVertexInMst[unvis_i]) continue;
+                        var weight = 0.0f;
+                        if (!EdgeExists(graph, vis_i, unvis_i, weight)) continue;
+                        if (weight >= edgeMinWeight) continue;
+                        edgeMinWeight = weight;
+                        minv1 = vis_i;
+                        minv2 = unvis_i;
+                    }
+                }
+
+                mst[minv1].Add(new EdgeAdjacent(minv2, edgeMinWeight));
+                // mst[minv2].Add(new EdgeAdjacent(minv1, edgeMinWeight));
+                isVertexInMst[minv2] = true;
+            }
+
+            return mst;
+        }
+    }
+}
---
+++ Assets/Scripts/MapGeneration/PathFinder.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Vec2i = UnityEngine.Vector2Int;
+using BlueRaja;
+using MapGeneration.MapPrimitives;
+
+namespace MapGeneration
+{
+    public class PathFinder
+    {
+        private class Grid
+        {
+            public Node[,] Data;
+            public Vec2i Size;
+
+            public Grid(Vec2i sz)
+            {
+                Data = new Node[sz.x, sz.y];
+                Size = sz;
+            }
+
+            public bool InBounds(Vec2i v)
+            {
+                return v.x < Size.x && v.y < Size.y &&
+                       v.x >= 0 && v.y >= 0;
+            }
+        }
+        public class Node
+        {
+            public Vec2i Position { get; private set; }
+            public Node Previous { get; set; }
+            public float Cost { get; set; }
+
+            public Node(Vec2i position)
+            {
+                Position = position;
+            }
+        }
+
+        static readonly Vec2i[] neighbors =
+        {
+            new Vec2i(1, 0),
+            new Vec2i(-1, 0),
+            new Vec2i(0, 1),
+            new Vec2i(0, -1),
+        };
+
+        private Grid gridNode;
+        private SimplePriorityQueue<Node, float> queue;
+        private HashSet<Node> visited;
+
+        public PathFinder(Vec2i size)
+        {
+            gridNode = new Grid(size);
+            queue = new SimplePriorityQueue<Node, float>();
+            visited = new HashSet<Node>();
+
+            for (var x = 0; x < size.x; x++)
+            {
+                for (var y = 0; y < size.y; y++)
+                {
+                    gridNode.Data[x, y] = new Node(new Vec2i(x, y));
+                }
+            }
+            ResetNodes();
+        }
+
+        private void ResetNodes()
+        {
+            var size = gridNode.Size;
+
+            for (var x = 0; x < size.x; x++)
+            {
+                for (var y = 0; y < size.y; y++)
+                {
+                    var node = gridNode.Data[x, y];
+                    node.Previous = null;
+                    node.Cost = float.PositiveInfinity;
+                }
+            }
+        }
+
+        private float FindCost(MapPrimitives.Grid grid, Node b, Vec2i endPos)
+        {
+            // var cost = Vec2i.Distance(b.Position, endPos) / 10;
+            var cost = 0;
+            // switch (grid.Data[b.Position.x, b.Position.y])
+            // {
+            //     case CellType.Room:
+            //         cost += 10;
+            //         break;
+            //     case CellType.None:
+            //         cost += 10;
+            //         break;
+            //     case CellType.Tunnel:
+            //         cost += 2;
+            //         break;
+            //     default:
+            //         throw new ArgumentOutOfRangeException();
+            // }
+            
+            switch (grid.Data[b.Position.x, b.Position.y])
+            {
+                case CellType.Room:
+                    cost += 10;
+                    break;
+                case CellType.None:
+                    cost += 10;
+                    break;
+                case CellType.Tunnel:
+                    cost += 2;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return cost;
+        }
+        
+        private List<Vec2i> ReconstructPath(Node node)
+        {
+            var stack = new Stack<Vec2i>();
+            var res = new List<Vec2i>();
+
+            while (node != null)
+            {
+                stack.Push(node.Position);
+                node = node.Previous;
+            }
+
+            while (stack.Count > 0)
+            {
+                res.Add(stack.Pop());
+            }
+
+            return res;
+        }
+
+        public List<Vec2i> FindPath(MapPrimitives.Grid grid, Vec2i start, Vec2i end)
+        {
+            ResetNodes();
+            queue.Clear();
+            visited.Clear();
+            
+            gridNode.Data[start.x, start.y].Cost = 0;
+            queue.Enqueue(gridNode.Data[start.x, start.y], 0);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                visited.Add(node);
+
+                if (node.Position == end)
+                {
+                    return ReconstructPath(node);
+                }
+
+                foreach (var ofs in neighbors)
+                {
+                    if (!gridNode.InBounds(node.Position + ofs)) continue;
+                    var neighborPos = node.Position + ofs;
+                    var neighbor = gridNode.Data[neighborPos.x, neighborPos.y];
+                    if (visited.Contains(neighbor)) continue;
+
+                    var neighborCost = node.Cost + FindCost(grid, neighbor, end);
+
+                    if (neighborCost < neighbor.Cost)
+                    {
+                        neighbor.Previous = node;
+                        neighbor.Cost = neighborCost;
+
+                        if (queue.TryGetPriority(node, out float existingPriority))
+                        {
+                            queue.UpdatePriority(node, neighborCost);
+                        }
+                        else
+                        {
+                            queue.Enqueue(neighbor, neighbor.Cost);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
